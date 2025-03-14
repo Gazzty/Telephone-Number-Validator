3795ade5f46075df9fffefa305de17659b7ed5a7
Gazzty <tomasgustavo99@gmail.com>
2025-03-14 15:56:39 -0300

    Added styles and started scripts

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get components
+const userInput = document.getElementById("user-input")
+const checkBtn = document.getElementById("checkBtn")
+const clearBtn = document.getElementById("clearBtn")
+const output = document.getElementById("results-div")
+
